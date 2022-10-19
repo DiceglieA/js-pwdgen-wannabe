253d326e65607f9de64efe2e37b82ed99a91d3f1
DiceglieA <angelodiceglie10@gmail.com>
2022-10-19 14:10:06 +0200

    variabili

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let nome = prompt(`il tuo nome: `)
+// console.log (nome)
+
+let surname = prompt(`il tuo cognome: `)
+// console.log (nome)
+
+let color = prompt(`il tuo colore preferito: `)
+// console.log (nome)
+
